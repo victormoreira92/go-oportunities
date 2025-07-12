a06fe4a935fcb0cb40db79e0fa2f929158596a12
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-07-11 23:32:09 -0300

    feat: Add Initialize handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,16 @@
 package handler
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/victormoreira92/go-oportunities/config"
+	"gorm.io/gorm"
 )
+
+var (
+	looger *config.Logger
+	db *gorm.DB
+)
+
+func InitializeHandler(){
+	looger = config.GetLogger("handler")
+	db = config.GetDb()
+}
--- router/routes.go
+++ router/routes.go
@@ -6,6 +6,9 @@ import (
 )
 
 func InitializeRoutes(router *gin.Engine){
+
+	handler.InitializeHandler()
+	
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOppeningHandler)
